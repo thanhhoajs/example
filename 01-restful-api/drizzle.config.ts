97a29454f5c6b30a8368db19488580921256361b
Nguyễn Như Khánh <kwalker.nnk@gmail.com>
2024-10-26 22:32:46 +0700

    fix: fix drizzle err

--- 01-restful-api/drizzle.config.ts
+++ 01-restful-api/drizzle.config.ts
@@ -1,15 +1,13 @@
 import { type Config, defineConfig } from 'drizzle-kit';
 
-import { dbConfig } from './src/configs/database.config';
-
 export default defineConfig({
-  dialect: dbConfig.type as Config['dialect'],
+  dialect: process.env.DB_TYPE as Config['dialect'],
   dbCredentials: {
-    host: dbConfig.host as string,
-    port: dbConfig.port,
-    user: dbConfig.username,
-    database: dbConfig.database as string,
-    password: dbConfig.password,
+    host: process.env.DB_HOST ?? 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
+    user: process.env.DB_USERNAME,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
   },
   schema: './src/database/schemas',
   out: './src/database/migrations',
